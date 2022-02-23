7e34e9b6d209b78f68b8381ac842d7eb21704164
vidhi-123 <vidhipshah10@gmail.com>
2022-02-23 12:49:27 +0530

    initial comiit

---
+++ src/Components/Blog.js
@@ -0,0 +1,16 @@
+
+
+const Blog=()=>{
+    return(
+        <>
+   
+    <div className='b1'>
+    <a target="_blank" href="https://www.instagram.com/shreeriddhibridalcollection/?utm_medium=copy_link" class="btn red block">Instagram</a>
+    </div>
+    <div className='b1'>
+    <a target="_blank" href="https://www.facebook.com/mahesh.suba.1" class="btn sky block">FaceBook</a>
+    </div>
+    </>
+    )
+}
+export default Blog
