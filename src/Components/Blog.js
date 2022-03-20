de753df01478b0280cc8d540a6f5e6c2c4a84305
Vidhi-123 <vidhipshah10@gmail.com>
2022-03-20 11:42:16 +0530

    payment added

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -15,6 +15,10 @@ const Blog=()=>{
     <a target="_blank" href="upi://pay?pa=paytmqr2810050501011h0kk8ms7m72@paytm&pn=Paytm%20Merchant&mc=5499&mode=02&orgid=000000&paytmqr=2810050501011H0KK8MS7M72&sign=MEYCIQCgfQq9rFZkt4KbDXWTdooJUjHHfBf5o10+ATJZwI6sFgIhALVT7T8zj11xgt8R2naLE2tc2vCvj4BaZ19uBNm5b5ZB" class="btn sky block">PAYTM payment</a>
     
     </div>
+    <div className='b1'>
+    <a target="_blank" href="8128240106@paytm" class="btn sky block">PAYTM demo</a>
+    
+    </div>
     </>
     )
 }
