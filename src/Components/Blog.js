1b04fac6d6d1483ec6dc828f1cbf1dff4f2a74ab
Vidhi-123 <vidhipshah10@gmail.com>
2022-03-20 11:28:39 +0530

    payment added

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -9,6 +9,11 @@ const Blog=()=>{
     </div>
     <div className='b1'>
     <a target="_blank" href="https://www.facebook.com/mahesh.suba.1" class="btn sky block">FaceBook</a>
+    
+    </div>
+    <div className='b1'>
+    <a target="_blank" href="upi://pay?pa=paytmqr2810050501011h0kk8ms7m72@paytm&pn=Paytm%20Merchant&mc=5499&mode=02&orgid=000000&paytmqr=2810050501011H0KK8MS7M72&sign=MEYCIQCgfQq9rFZkt4KbDXWTdooJUjHHfBf5o10+ATJZwI6sFgIhALVT7T8zj11xgt8R2naLE2tc2vCvj4BaZ19uBNm5b5ZB" class="btn sky block">PAYTM payment</a>
+    
     </div>
     </>
     )
