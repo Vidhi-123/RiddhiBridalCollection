abb3800c7688240cb1c4a238b53bce143a5f07f4
Vidhi-123 <vidhipshah10@gmail.com>
2022-03-24 18:56:39 +0530

    aaded

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,24 +1,57 @@
+import { Col, Row } from "antd"
 
-
+import bg from '../images/logo.jpeg'
 const Blog=()=>{
     return(
         <>
-   
-    <div className='b1'>
+        <Row style={{marginTop:"50px"}}>
+            <Col span={24}>
+                <img src={bg} alt="logo" height={150} width={180} />
+            </Col>
+        </Row>
+        <Row style={{marginTop:"30px"}}>
+
+            <Col span={24}>
+              
+            <div className='b1'>
     <a target="_blank" href="https://www.instagram.com/shreeriddhibridalcollection/?utm_medium=copy_link" class="btn red block">Instagram</a>
     </div>
     <div className='b1'>
     <a target="_blank" href="https://www.facebook.com/mahesh.suba.1" class="btn sky block">FaceBook</a>
     
     </div>
-    <div className='b1'>
-    <a target="_blank" href="upi://pay?pa=paytmqr2810050501011h0kk8ms7m72@paytm&pn=Paytm%20Merchant&mc=5499&mode=02&orgid=000000&paytmqr=2810050501011H0KK8MS7M72&sign=MEYCIQCgfQq9rFZkt4KbDXWTdooJUjHHfBf5o10+ATJZwI6sFgIhALVT7T8zj11xgt8R2naLE2tc2vCvj4BaZ19uBNm5b5ZB" class="btn sky block">PAYTM payment</a>
+                </Col>
+        </Row>
+        <Row style={{marginTop:"30px",textAlign:"left",marginLeft:"30px"}}>
+            <Col span={24}>
+                    
+                    {/* <span className="label">Mahesh Thakkar</span> */}
+                    <span className="label">Contact Details</span>
+                </Col>
+
+                <Col span={24}>
+                    
+                    {/* <span className="label">Mahesh Thakkar</span> */}
+                    <span style={{color:"white",fontSize:"22px"}}>Mahesh Thakkar: 9825352053</span>
+                </Col>
+
+                <Col span={24}>
+                    
+                    {/* <span className="label">Mahesh Thakkar</span> */}
+                    <span style={{color:"white",fontSize:"22px"}}>Mittal Thakkar: 9510774544</span>
+                </Col>
+                
+        </Row>
+        <Row style={{marginTop:"20px",textAlign:"left",marginLeft:"30px"}}>
+        <Col span={24} style={{color:"white",fontSize:"22px"}}>
+                    
+                    {/* <span className="label">Mahesh Thakkar</span> */}
+                    <span className="label">Address</span>
+                    <br />
+                    <span>213, Yashnidhi Appartment, 29, Saraswati society, Paldi, Ahmedabad-380007</span>
+                </Col>
+        </Row>
     
-    </div>
-    <div className='b1'>
-    <a target="_blank" href="8128240106@paytm" class="btn sky block">PAYTM demo</a>
-    
-    </div>
     </>
     )
 }
